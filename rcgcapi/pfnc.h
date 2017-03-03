dac7f34905201f4df55e7aabbbbc42a463aab0b4
Heiko Hirschmueller <heiko.hirschmueller@roboception.de>
2017-03-03 07:21:01 +0100

    Added pfnc.h with custom image format Error8

---
+++ rcgcapi/pfnc.h
@@ -0,0 +1,24 @@
+/*
+ * Roboception GmbH
+ * Munich, Germany
+ * www.roboception.com
+ *
+ * Copyright (c) 2017 Roboception GmbH
+ * All rights reserved
+ *
+ * Author: Heiko Hirschmueller
+ */
+
+#ifndef RCGCAPI_PFNC
+#define RCGCAPI_PFNC
+
+// definition of standard pixel formats
+
+#define PFNC_INCLUDE_HELPERS
+#include <GenTL/PFNC.h>
+
+// definition of Roboception format for error with 8 bit
+
+#define Error8 0x81080001 /* Error 8-bit */
+
+#endif
