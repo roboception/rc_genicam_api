fb7a6a8554af94af5abfe70e0854d06f0efcb846
Heiko Hirschmueller <heiko.hirschmueller@roboception.de>
2018-10-09 22:22:28 +0200

    Added helper function for determiation of component from buffer part index

--- rc_genicam_api/config.h
+++ rc_genicam_api/config.h
@@ -288,6 +288,29 @@ void checkFeature(const std::shared_ptr<GenApi::CNodeMapRef> &nodemap, const cha
 std::shared_ptr<GenApi::CChunkAdapter> getChunkAdapter(const std::shared_ptr<GenApi::CNodeMapRef> &nodemap,
                                                        const std::string &tltype);
 
+
+/**
+  Returns the component name for the given part index of the buffer. It is
+  expected that chunk data has been enabled and the buffer that corresponds to
+  the part indes has been attached to the nodemap.
+
+  If this is not the case, or if the chunk data does not contain required
+  parameters, then the component name is guessed from the pixel format of the
+  requested part. The heuristic of this is designed for Roboceptions rc_visard.
+
+  @param nodemap Feature nodemap that should already have been attached to the
+                 buffer.
+  @param buffer  Buffer that should already have been attached to the nodemap.
+  @param part    Part index of buffer for which the component name is requested.
+  @return        Component name, which may be derived from the pixel format or
+                 an empty string if it cannot be determined.
+*/
+
+class Buffer;
+
+std::string getComponetOfPart(const std::shared_ptr<GenApi::CNodeMapRef> &nodemap,
+                              const Buffer *buffer, int part);
+
 }
 
 #endif
